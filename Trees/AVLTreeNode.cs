c7d814bf5a1a9c9e12bfbd66d61aabc7f422b5ec
MariiaBel <stepina.m.p@gmail.com>
2019-12-06 08:55:29 +0300

    AVL tree add nodes, getEnumerator

---
+++ Trees/AVLTreeNode.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    class AVLTreeNode<TNode> : IComparable<TNode> where TNode : IComparable
+    {
+        AVLTree<TNode> tree;
+        AVLTreeNode<TNode> left,
+                           right;
+
+        public  AVLTreeNode(TNode value, AVLTreeNode<TNode> parent, AVLTree<TNode> tree)
+        {
+            this.Value = value;
+            this.Parent = parent;
+            this.tree = tree;
+        }
+
+        public AVLTreeNode<TNode> Left
+        {
+            get
+            {
+                return left;
+            }
+            internal set
+            {
+                left = value;
+                if (left != null) left.Parent = this;
+            }
+        }
+
+        public AVLTreeNode<TNode> Right
+        {
+            get
+            {
+                return right;
+            }
+            internal set
+            {
+                right = value;
+                if (right != null) right.Parent = this;
+            }
+
+        }
+
+        public AVLTreeNode<TNode> Parent
+        {
+            get;
+            internal set;
+        }
+
+        public TNode Value
+        {
+            get;
+            private set;
+        }
+
+        public int CompareTo(TNode other)
+        {
+            return Value.CompareTo(other);
+        }
+
+    }
+}
---
+++ Trees/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AVLTree<int> tree = new AVLTree<int>();
+
+            //tree.Add(1);
+            //tree.Add(6);
+            //tree.Add(2);
+            //tree.Add(3);
+            //tree.Add(4);
+            //tree.Add(5);
+
+            tree.Add(new int[] { 1, 2, 5, 7, 9, 3, 4, 2, 1, 4 });
+
+            foreach (var item in tree)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
